397e5e1fcc14c7beb2991babd5da3e789ff67081
Sebastian Ayala Gonzalez <sebaxayala@gmail.com>
2022-06-25 21:04:15 -0500

    Implement http methods for management Jokes

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,12 @@ ENV CHUCK="https://api.chucknorris.io/"
 ENV DAD="https://icanhazdadjoke.com/"
 ENV FLASK_ENV="development"
 ENV APP_SETTINGS_MODULE="config.default.Development"
-RUN apk add gcc musl-dev python3-dev libffi-dev openssl-dev
+ENV MYSQL_DATABASE_USER="root"
+ENV MYSQL_DATABASE_PASSWORD="root"
+ENV MYSQL_DATABASE_DB="squadmakers"
+ENV MYSQL_DATABASE_HOST="db"
+RUN apk add --no-cache gcc musl-dev python3-dev libffi-dev openssl-dev
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 EXPOSE 80
--- app/resources/joke.py
+++ app/resources/joke.py
@@ -1,12 +1,34 @@
+import json
+from flask import jsonify, request
 from flask_restful import Resource
 from app.adapters.general_joke_adapter import JokeEspecificRequest
+from app.models.joke import Joke as JokeModel
 import random
 
 
+class Joke(Resource):
 
-class RandomJoke(Resource):
     def get(self, origin=None):
         if not origin:
             keys_dict = list(JokeEspecificRequest.joke_origins.keys())
             origin = random.choice(keys_dict)
         return JokeEspecificRequest.get_joke(origin)
+
+    def post(self, origin=None):
+        json_data = request.get_json(force=True)
+        joke_text = json_data["joke_text"]
+        joke_obj = JokeModel(joke_text)
+        return joke_obj.save()
+
+    def put(self, origin=None):
+        json_data = request.get_json(force=True)
+        joke_id = json_data["id"]
+        joke_text = json_data["joke_text"]
+        joke_obj = JokeModel(joke_text, joke_id)
+        return joke_obj.update()
+
+    def delete(self, origin=None):
+        json_data = request.get_json(force=True)
+        joke_id = json_data["id"]
+        joke_obj = JokeModel(_id=joke_id)
+        return joke_obj.delete()
